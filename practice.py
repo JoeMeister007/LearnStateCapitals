b698637b6bf2db2e7fed85f93bfdbaea399cdadd
JoeMeister007 <joemeister007@gmail.com>
2024-04-29 22:38:05 -0400

    Improved
    Made less random so you can go through all the states. Random chance to get previous ones, higher odds of getting one you got wrong

--- practice.py
+++ practice.py
@@ -7,17 +7,54 @@ def multiple_choice():
     print("Type the NUMBER of the correct answer")
     print("Type q to quit")
 
+    #get all the states in a random order
+    state_queue = []
+    correct_list = []
+    incorrect_list = []
+
+    correct_redo_prob = 0.05 #the probability of getting a previous correct question
+    incorrect_redo_prob = 0.20 #the probability of getting a previous incorrect question
     while True:
-        #pick random states
-        random_states = random.sample(capital_info.states, k=3)
-        state = random_states[0] #the first is ours
+        #if we've gone through all the states reset
+        if len(state_queue) == 0 :
+            state_queue = capital_info.states.copy()
+            random.shuffle(state_queue)
+
+            correct_list = []
+            incorrect_list = []
+        redo_rand = random.random()
+
+        #there's a probability that we redo an answer we got correct
+        if redo_rand < correct_redo_prob:
+            if len(correct_list) > 0:
+                state_queue.insert(0, correct_list.pop(0))
+        #that didn't happen
+        else:
+            redo_rand -= correct_redo_prob
+            #there's a probability that we redo an answer we got wrong
+            if redo_rand < incorrect_redo_prob:
+                if len(incorrect_list) > 0:
+                    state_queue.insert(0, incorrect_list.pop(0))
+
+        #get our state from the front of the queue
+        state = state_queue.pop(0)
         #add the correct capital to our list of cities
         cities = [state["capital"]]
         #add a decoy from the same state
         cities.append(random.choice(state["other_cities"]))
+        
+        
+        #pick some other random states (3 cause one might be ours)
+        random_states = random.sample(capital_info.states, k=3)
+
         #add capitals from two other states
-        for i in range(1,len(random_states)):
+        for i in range(0,len(random_states)):
+            if random_states[i]["name"] ==state["name"]:
+                continue
+
             cities.append(random_states[i]["capital"])
+            if len(cities) >= 4:
+                break
 
         #shuffle it up
         random.shuffle(cities)
@@ -35,8 +72,10 @@ def multiple_choice():
             idx = int(guess)
             if cities[idx - 1] == state["capital"]:
                 print("Correct!")
+                correct_list.append(state)
             else:
                 print("Incorrect. It's " + state["capital"])
+                incorrect_list.append(state)
         except:
             print("Incorrect. It's " + state["capital"])
         
@@ -48,9 +87,37 @@ def short_answer():
     print("Type out the correct capital")
     print("Type q to quit")
 
+    #get all the states in a random order
+    state_queue = []
+    correct_list = []
+    incorrect_list = []
+
+    correct_redo_prob = 0.05 #the probability of getting a previous correct question
+    incorrect_redo_prob = 0.20 #the probability of getting a previous incorrect question
     while True:
-        #pick random states
-        state = random.choice(capital_info.states)
+        #if we've gone through all the states reset
+        if len(state_queue) == 0 :
+            state_queue = capital_info.states.copy()
+            random.shuffle(state_queue)
+
+            correct_list = []
+            incorrect_list = []
+        redo_rand = random.random()
+
+        #there's a probability that we redo an answer we got correct
+        if redo_rand < correct_redo_prob:
+            if len(correct_list) > 0:
+                state_queue.insert(0, correct_list.pop(0))
+        #that didn't happen
+        else:
+            redo_rand -= correct_redo_prob
+            #there's a probability that we redo an answer we got wrong
+            if redo_rand < incorrect_redo_prob:
+                if len(incorrect_list) > 0:
+                    state_queue.insert(0, incorrect_list.pop(0))
+
+        #get our state from the front of the queue
+        state = state_queue.pop(0)
 
         #ready to roll
         print()
@@ -61,8 +128,10 @@ def short_answer():
             break
         elif guess.lower() == state["capital"].lower():
             print("Correct!")
+            correct_list.append(state)
         else:
             print("Incorrect. It's " + state["capital"])
+            incorrect_list.append(state)
 
 print("""Welcome! This program helps you memorize the state captials.
 It will give you a state and you must list the capital.""")
